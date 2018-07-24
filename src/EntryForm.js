8cf71cfb5bfc0b3393e6e39c76d8a7ae5dd071f0
Abhishek Thakur <abhishek.thakur@infoobjects.com>
2018-07-24 19:36:23 +0530

    reload issue fixed

--- src/EntryForm.js
+++ src/EntryForm.js
@@ -6,29 +6,30 @@ class EntryForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            applicantName: '',
-            applicantType: '',
-            applicantAddress: '',
-            applicantContact: '',
-            buildingName: '',
-            buildingAddress: '',
-            area:'',
-        };
+            fields: {
+                applicantName: '',
+                applicantType: '',
+                applicantAddress: '',
+                applicantContact: '',
+                buildingName: '',
+                buildingAddress: '',
+                area:'',
+            }
+        }
 
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleChange(event) {
-        this.setState({
-            applicantName: this.refs.applicantName.value,
-            applicantType: this.refs.applicantType.value,
-            applicantAddress: this.refs.applicantAddress.value,
-            applicantContact: this.refs.applicantContact.value,
-            buildingName: this.refs.buildingName.value,
-            buildingAddress: this.refs.buildingAddress.value,
-            area: this.refs.area.value
-        });
+        const element = event.nativeEvent.target;
+        this.setState((prevState) => ({
+            ...prevState,
+            fields: {
+                ...prevState.fields,
+                [element.name]: element.value
+            }
+        }));
     }
 
     handleSubmit(event) {
@@ -42,95 +43,111 @@ class EntryForm extends React.Component {
                 <Row>
                     <section className="col-xs-12">
                         <form onSubmit={this.handleSubmit}>
-                            <FormGroup bsSize="small" >
-                                <ControlLabel htmlFor="applicantName">Applicant Name</ControlLabel>
-                                <FormControl
-                                    type="text"
-                                    autoComplete="on"
-                                    name="applicantName"
-                                    ref="applicantName"
-                                    value={this.state.applicantName}
-                                    onChange={this.handleChange}
-                                />
-                            </FormGroup>
+                            <Col md={4}>
+                                <FormGroup md={4} bsSize="large" >
+                                    <ControlLabel htmlFor="applicantName">Applicant Name</ControlLabel>
+                                    <FormControl
+                                        type="text"
+                                        autoComplete="on"
+                                        name="applicantName"
+                                        ref="applicantName"
+                                        value={this.state.fields.applicantName}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                            </Col>
 
-                            <FormGroup bsSize="small" >
-                                <ControlLabel htmlFor="applicantAddress">Applicant Address</ControlLabel>
-                                <FormControl
-                                    type="text"
-                                    autoComplete="on"
-                                    name="applicantAddress"
-                                    ref="applicantAddress"
-                                    value={this.state.applicantAddress}
-                                    onChange={this.handleChange}
-                                />
-                            </FormGroup>
+                            <Col md={4}>
+                                <FormGroup bsSize="large" >
+                                    <ControlLabel htmlFor="applicantAddress">Applicant Address</ControlLabel>
+                                    <FormControl
+                                        type="text"
+                                        autoComplete="on"
+                                        name="applicantAddress"
+                                        ref="applicantAddress"
+                                        value={this.state.fields.applicantAddress}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                            </Col>
 
-                            <FormGroup bsSize="small" >
-                                <ControlLabel htmlFor="applicantContact">Applicant Contact</ControlLabel>
-                                <FormControl
-                                    type="text"
-                                    autoComplete="on"
-                                    name="applicantContact"
-                                    ref="applicantContact"
-                                    value={this.state.applicantContact}
-                                    onChange={this.handleChange}
-                                />
-                            </FormGroup>
+                            <Col md={4}>
+                                <FormGroup bsSize="large" >
+                                    <ControlLabel htmlFor="applicantContact">Applicant Contact</ControlLabel>
+                                    <FormControl
+                                        type="text"
+                                        autoComplete="on"
+                                        name="applicantContact"
+                                        ref="applicantContact"
+                                        value={this.state.fields.applicantContact}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                            </Col>
 
-                            <FormGroup bsSize="small" >
-                                <ControlLabel htmlFor="applicantContact">Building Name</ControlLabel>
-                                <FormControl
-                                    type="text"
-                                    autoComplete="on"
-                                    name="buildingName"
-                                    ref="buildingName"
-                                    value={this.state.buildingName}
-                                    onChange={this.handleChange}
-                                />
-                            </FormGroup>
+                            <Col md={4}>
+                                <FormGroup bsSize="large" >
+                                    <ControlLabel htmlFor="applicantContact">Building Name</ControlLabel>
+                                    <FormControl
+                                        type="text"
+                                        autoComplete="on"
+                                        name="buildingName"
+                                        ref="buildingName"
+                                        value={this.state.fields.buildingName}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                            </Col>
 
-                            <FormGroup bsSize="small" >
-                                <ControlLabel htmlFor="applicantContact">Building Address</ControlLabel>
-                                <FormControl
-                                    type="text"
-                                    autoComplete="on"
-                                    name="buildingAddress"
-                                    ref="buildingAddress"
-                                    value={this.state.buildingAddress}
-                                    onChange={this.handleChange}
-                                />
-                            </FormGroup>
+                            <Col md={4}>
+                                <FormGroup bsSize="large" >
+                                    <ControlLabel htmlFor="applicantContact">Building Address</ControlLabel>
+                                    <FormControl
+                                        type="text"
+                                        autoComplete="on"
+                                        name="buildingAddress"
+                                        ref="buildingAddress"
+                                        value={this.state.fields.buildingAddress}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                            </Col>
 
-                            <FormGroup bsSize="small" >
-                                <ControlLabel htmlFor="applicantContact">Area</ControlLabel>
-                                <FormControl
-                                componentClass="select"
-                                name="area"
-                                ref="area"
-                                value={this.state.area}
-                                onChange={this.handleChange}>
-                                    <option value="">Select</option>
-                                    <option value="urban">Urban</option>
-                                    <option value="rural">Rural</option>
-                                </FormControl>
-                            </FormGroup>
+                            <Col md={4}>
+                                <FormGroup bsSize="large" >
+                                    <ControlLabel htmlFor="applicantContact">Area</ControlLabel>
+                                    <FormControl
+                                    componentClass="select"
+                                    name="area"
+                                    ref="area"
+                                    value={this.state.fields.area}
+                                    onChange={this.handleChange}>
+                                        <option value="">Select</option>
+                                        <option value="urban">Urban</option>
+                                        <option value="rural">Rural</option>
+                                    </FormControl>
+                                </FormGroup>
+                            </Col>
 
-                            <FormGroup bsSize="small" >
-                                <ControlLabel htmlFor="applicantContact">Applicant Type</ControlLabel>
-                                <FormControl
-                                componentClass="select"
-                                name="applicantType"
-                                ref="applicantType"
-                                value={this.state.applicantType}
-                                onChange={this.handleChange}>
-                                    <option value="">Select</option>
-                                    <option value="urban">Permanent</option>
-                                    <option value="rural">Temporary</option>
-                                </FormControl>
-                            </FormGroup>
+                            <Col md={4}>
+                                <FormGroup bsSize="large" >
+                                    <ControlLabel htmlFor="applicantContact">Applicant Type</ControlLabel>
+                                    <FormControl
+                                    componentClass="select"
+                                    name="applicantType"
+                                    ref="applicantType"
+                                    value={this.state.fields.applicantType}
+                                    onChange={this.handleChange}>
+                                        <option value="">Select</option>
+                                        <option value="urban">Permanent</option>
+                                        <option value="rural">Temporary</option>
+                                    </FormControl>
+                                </FormGroup>
+                            </Col>
 
-                            <Button type="submit" className="btn btn-default">Submit</Button>
+                            <Col md={12}>
+                                <Button type="submit" className="btn btn-default">Submit</Button>
+                            </Col>
                         </form>
                     </section>
                 </Row>
