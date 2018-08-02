70683a22f60cc677346daeaffe49fb16ade24aa0
Abhishek Thakur <abhishek.thakur@infoobjects.com>
2018-08-02 11:30:45 +0530

    status code fix

--- router/users.js
+++ router/users.js
@@ -19,9 +19,9 @@ users.post('/register', function(req, res) {
 	    }
 		connection.query(`INSERT INTO ${process.env.USER_TBL} SET ?`, userData, function(err, results, fields) {
 			if (err) {
-				res.status(400).json({'message' : err})
+				res.status(200).json({'message' : err})
 			}
-			res.status(400).json({'message' : 'User Registered successfully'})
+			res.status(200).json({'message' : 'User Registered successfully'})
 		})
 		connection.end(err => {
 			if(err) console.log(err);
@@ -39,7 +39,7 @@ users.post('/login', function(req, res) {
     var token = '';
 	connection.query(`SELECT * FROM ${process.env.USER_TBL} WHERE EMAIL = ?`, [email], function(err, results, fields) {
 		if (err) {
-			res.status(400).json({'message' : err, 'token' : token})
+			res.status(200).json({'message' : err, 'token' : token})
 		}
 		if (results.length > 0) {
 			bcrypt.compare(password, results[0].PASSWORD).then(function(match) {
@@ -49,11 +49,11 @@ users.post('/login', function(req, res) {
 					})
 					res.status(200).json({'message' : 'User verified', 'token' : token})
 				} else {
-					res.status(204).json({'message' : 'Email or Password does not match', 'token' : token})
+					res.status(200).json({'message' : 'Email or Password does not match', 'token' : token})
 				}
 			})
 		} else {
-			res.status(204).json({'message' : 'Email does not exists', 'token' : token})
+			res.status(200).json({'message' : 'Email does not exists', 'token' : token})
 		}
 	})
 	connection.end(err => {
@@ -66,13 +66,13 @@ users.use(function(req, res, next) {
  	if (token) {
 		jwt.verify(token, process.env.SECRET_KEY, function(err) {
 			if (err) {
-	 			res.status(500).json({'message' : err})
+	 			res.status(200).json({'message' : err})
 			} else {
 				next();
 	 		}
  		});
  	} else {
-		res.status(403).json({'message' : 'Please send a token'})
+		res.status(200).json({'message' : 'Please send a token'})
  	}
 })
 
