b89cc35cc02273ce341a1d008a37822669294460
Abhishek Thakur <abhishek.thakur@infoobjects.com>
2018-08-06 19:05:28 +0530

    server side validation added

--- router/records.js
+++ router/records.js
@@ -1,33 +1,50 @@
 const express = require('express')
 const records = express.Router()
 const connection = require('../db/dbConnection')
+const { check, validationResult } = require('express-validator/check')
 
 /* 	path: /register
  *	type: POST 
  */
 
-records.post('/addNewRecord', function(req, res) {
-	//need to add jwt token in authorization header with this request.
-	const fileRecordData = {
-        "APPLICANT_NAME": req.body.applicant_name,
-        "APPLICANT_TYPE": req.body.applicant_type,
-        "APPLICANT_ADDRESS": req.body.applicant_address,
-        "APPLICANT_CONTACT": req.body.applicant_contact,
-        "BUILDING_NAME": req.body.building_name,
-        "BUILDING_ADDRESS": req.body.building_address,
-        "BUILDING_AREA": req.body.building_area,
-        "FILE_NUMBER": req.body.file_number,
-        "REMARK": req.body.remark,
-    }
-	connection.query(`INSERT INTO ${process.env.FILE_RECORD_TBL} SET ?`, fileRecordData, function(err, results, fields) {
-		if (err) {
-			res.status(200).json({'message' : err, 'saved' : false})
-		}
-		res.status(200).json({'message' : 'Record saved successfully', 'saved' : true})
-	})
-	// connection.end(err => {
-	// 	if(err) console.log(err);
-	// })
-})
+records.post('/addNewRecord',
+	[
+		check('applicant_name').not().isEmpty().withMessage('Applicant name cannot be empty').trim().escape(),
+		check('applicant_type').not().isEmpty().withMessage('Please select an applicant type').trim().escape(),
+		check('applicant_address').not().isEmpty().withMessage('Applicant address cannot be empty').trim().escape(),
+		check('applicant_contact').not().isEmpty().withMessage('Applicant contact cannot be empty').trim().escape(),
+		check('building_name').not().isEmpty().withMessage('Building name cannot be empty').trim().escape(),
+		check('building_address').not().isEmpty().withMessage('Building Address cannot be empty').trim().escape(),
+		check('building_area').not().isEmpty().withMessage('Building Area cannot be empty').trim().escape(),
+		check('file_number').not().isEmpty().withMessage('File number cannot be empty').trim().escape(),
+		check('remark').trim().escape()
+	],
+	function(req, res) {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+		    res.status(200).json({message: errors.array(), saved : false})
+	  	}
+		const fileRecordData = {
+	        "APPLICANT_NAME": req.body.applicant_name,
+	        "APPLICANT_TYPE": req.body.applicant_type,
+	        "APPLICANT_ADDRESS": req.body.applicant_address,
+	        "APPLICANT_CONTACT": req.body.applicant_contact,
+	        "BUILDING_NAME": req.body.building_name,
+	        "BUILDING_ADDRESS": req.body.building_address,
+	        "BUILDING_AREA": req.body.building_area,
+	        "FILE_NUMBER": req.body.file_number,
+	        "REMARK": req.body.remark,
+	    }
+		connection.query(`INSERT INTO ${process.env.FILE_RECORD_TBL} SET ?`, fileRecordData, function(err, results, fields) {
+			if (err) {
+				res.status(200).json({message : err, saved : false})
+			}
+			res.status(200).json({message : 'Record saved successfully', saved : true})
+		})
+		// connection.end(err => {
+		// 	if(err) console.log(err);
+		// })
+	}
+)
 
 module.exports = records
--- router/users.js
+++ router/users.js
@@ -2,32 +2,43 @@ const express = require('express')
 const users = express.Router()
 const connection = require('../db/dbConnection')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const bcrypt = require('bcrypt')
+const { check, validationResult } = require('express-validator/check')
+const saltRounds = 10
 
 /* 	path: /register
  *	type: POST 
  */
 
-users.post('/register', function(req, res) {
-	bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
-		const userData = {
-	        "USER_NAME": req.body.user_name,
-	        "EMAIL": req.body.email,
-	        "PASSWORD": hash,
-	        "ROLE": req.body.role,
-	    }
-		connection.query(`INSERT INTO ${process.env.USER_TBL} SET ?`, userData, function(err, results, fields) {
-			if (err) {
-				res.status(200).json({'message' : err})
-			}
-			res.status(200).json({'message' : 'User Registered successfully'})
+users.post('/register', 
+	[
+		check('user_name').trim().escape(),
+		check('email').isEmail().withMessage('email is not a valid email'),
+		check('password').isLength({ min: 8 }).withMessage('password must be atleast 8 characters in length'),
+		check('role').trim().escape()
+	],
+	function(req, res) {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+		    res.status(200).json({message: errors.array()})
+	  	}
+
+		bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
+			const userData = {
+		        "USER_NAME": req.body.user_name,
+		        "EMAIL": req.body.email,
+		        "PASSWORD": hash,
+		        "ROLE": req.body.role,
+		    }
+			connection.query(`INSERT INTO ${process.env.USER_TBL} SET ?`, userData, function(err, results, fields) {
+				if (err) {
+					res.status(200).json({message : err})
+				}
+				res.status(200).json({message : 'User Registered successfully'})
+			})
 		})
-		connection.end(err => {
-			if(err) console.log(err);
-		})
-	})
-})
+	}
+)
 
 /* 	path: /login
  *	type: POST 
