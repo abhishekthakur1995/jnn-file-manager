635b0accaa27d36b7493b10603423a5d71b042ad
abhishek <coach.ab9@gmail.com>
2018-09-06 12:03:36 +0530

    Update dbConnection.js

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,19 +1,28 @@
 var mysql = require('mysql')
 
+// var dbConnection = mysql.createConnection({
+// 	host     : process.env.DB_HOST,
+//   	user     : process.env.DB_USER,
+//   	password : process.env.DB_PASSWORD,
+//   	database : process.env.DB_NAME,
+//   	charset  : process.env.DB_CHARSET
+// })
+
+// OPENSHIFT
+
 var dbConnection = mysql.createConnection({
-	host     : process.env.OPENSHIFT_MYSQL_DB_HOST || process.env.DB_HOST,
-  	user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME || process.env.DB_USER,
-  	password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD || process.env.DB_PASSWORD,
-  	database : process.env.OPENSHIFT_APP_NAME || process.env.DB_NAME,
-  	charset  : process.env.DB_CHARSET || 'utf8mb4'
+    host     : getenv('MYSQL_SERVICE_HOST'),
+    user     : getenv('databaseusername'),
+    password : getenv('databasepassword'),
+    database : getenv('databasename')
 })
 
 dbConnection.connect(function(err) {
 	if (err) {
-    	console.error('error connecting: ' + err.stack);
-    	return;
+    	console.error('error connecting: ' + err.stack)
+    	return
   	}
-  	console.log('connected as id ' + dbConnection.threadId);
-});
+  	console.log('connected as id ' + dbConnection.threadId)
+})
 
-module.exports = dbConnection;
+module.exports = dbConnection
