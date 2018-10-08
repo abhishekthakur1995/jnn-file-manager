72cf65ddbcf45d61e86d3d487084566e9b94e6a6
Abhishek Thakur <abhishek.thakur@infoobjects.com>
2018-10-08 16:21:31 +0530

    #new error page design

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Grid } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 class ErrorPage extends React.Component {
 	render() {
 	    return (
-	        <Grid className="jumbotron text-align-center">
-	            <h1 className="display-3">404</h1>
-	        </Grid>
+	        <Grid bsClass="error-page">
+	        	<Grid id="notfound">
+	        		<Grid bsClass="notfound">
+	        			<Grid bsClass="notfound-404">
+	        				<h1>404</h1>
+	        			</Grid>
+	        			<h2>Oops! This Page Could Not Be Found</h2>
+	        			<p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+	        			<Link to="/servicePanel">Go Back To Dashboard</Link>
+	        		</Grid>
+	        	</Grid>
+        	</Grid>
 	    )
 	}
 }
+
 export default ErrorPage
