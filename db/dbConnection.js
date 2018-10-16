b4caf78241d4fe641315f31d68652744d6f2183b
Abhishek Thakur <abhishek.thakur@infoobjects.com>
2018-10-16 16:16:43 +0530

    #added console

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql')
 
 const dbConnection = mysql.createConnection({
-	host     : process.env.DB_HOST || process.env.MYSQL_SERVICE_HOST,
-  	user     : process.env.DB_USER || process.env.databaseusername,
-  	password : process.env.DB_PASSWORD || process.env.databasepassword,
-  	database : process.env.DB_NAME || process.env.databasename,
-  	charset  : process.env.DB_CHARSET
+    host     : process.env.DB_HOST || process.env.MYSQL_SERVICE_HOST,
+    user     : process.env.DB_USER || process.env.databaseusername,
+    password : process.env.DB_PASSWORD || process.env.databasepassword,
+    database : process.env.DB_NAME || process.env.databasename,
+    charset  : process.env.DB_CHARSET
 })
 
 dbConnection.connect(function(err) {
-	if (err) {
-    	console.error('error connecting: ' + err.stack)
-    	return
-  	}
-  	console.log('connected as id ' + dbConnection.threadId)
+    if (err) {
+        console.error('error connecting: ' + err.stack)
+        return
+    }
+    console.log(dbConnection)
+    console.log('connected as id ' + dbConnection.threadId)
 })
 
 module.exports = dbConnection
