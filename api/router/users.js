f629d294db190df805b1d571c05586d87c7da479
Abhishek Thakur <abhishek.thakur@infoobjects.com>
2018-10-17 12:06:01 +0530

    #modified api messages.

--- api/router/users.js
+++ api/router/users.js
@@ -11,15 +11,15 @@ const { check, validationResult } = require('express-validator/check')
 
 users.post('/register',
 	[
-		check('user_name').not().isEmpty().withMessage('user name cannot be empty').trim().escape(),
-		check('email').isEmail().withMessage('email is not a valid email').trim().escape(),
-		check('password').isLength({ min: 8 }).withMessage('password must be atleast 8 characters in length'),
-		check('role').not().isEmpty().withMessage('role cannot be empty').trim().escape()
+		check('user_name').not().isEmpty().withMessage('User name cannot be empty').trim().escape(),
+		check('email').isEmail().withMessage('Email is not a valid email').trim().escape(),
+		check('password').isLength({ min: 8 }).withMessage('Password must be atleast 8 characters in length'),
+		check('role').not().isEmpty().withMessage('Role cannot be empty').trim().escape()
 	],
 	(req, res) => {
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
-		    return res.status(400).json({message: errors.array()})
+		    return res.status(400).json({message: errors.array()[0].msg, saved: false})
 	  	}
 
 		bcrypt.hash(req.body.password, parseInt(process.env.SALT_ROUNDS)).then((hash) => {
@@ -31,9 +31,13 @@ users.post('/register',
 		    }
 			connection.query(`INSERT INTO ${process.env.USER_TBL} SET ?`, userData, (err, results, fields) => {
 				if (err) {
-					return res.status(400).json({message : err})
+					if(err.code === 'ER_DUP_ENTRY') {
+						return res.status(400).json({message: 'User already exists.', saved: false})
+					} else {
+						return res.status(400).json({message: 'Unable to create user. Please try again', saved: false})
+					}
 				}
-				res.status(200).json({message : 'User Registered successfully'})
+				res.status(200).json({message: 'User Registered successfully', saved: true})
 			})
 		})
 	}
@@ -45,20 +49,20 @@ users.post('/register',
 
 users.post('/login',
 	[
-		check('email').not().isEmpty().withMessage('email cannot be empty'),
-		check('password').not().isEmpty().withMessage('password cannot be empty')
+		check('email').not().isEmpty().withMessage('Email address cannot be empty. Please enter an email address'),
+		check('password').not().isEmpty().withMessage('Password cannot be empty. Please enter your password')
 	],
 	(req, res) => {
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
-		    return res.status(400).json({message: errors.array()})
+		    return res.status(400).json({message: errors.array()[0].msg, success: false})
 	  	}
 
 		const email = req.body.email;
 	    const password = req.body.password;
 	    let token = '';
 		connection.query(`SELECT * FROM ${process.env.USER_TBL} WHERE EMAIL = ? AND STATUS = 1`, [email], (err, results, fields) => {
-			if (err) { return res.status(400).json({message : err, token : token, success : false}) }
+			if (err) { return res.status(400).json({message: err, token: token, success: false}) }
 
 			if (results.length > 0) {
 				bcrypt.compare(password, results[0].PASSWORD).then((match) => {
@@ -67,13 +71,13 @@ users.post('/login',
 				 			expiresIn: parseInt(process.env.TOKEN_EXPIRY_TIME)
 						})
 						req.session[token] = results[0].ROLE
-						return res.status(200).json({message : 'Login Successful. You will be redirected to the dashboard.', 'userRole': results[0].ROLE , token: token, validUpto : Date.now() + parseInt(process.env.TOKEN_EXPIRY_TIME), success : true})
+						return res.status(200).json({message: 'Login Successful. You will be redirected to the dashboard.', userRole: results[0].ROLE , token: token, validUpto: Date.now() + parseInt(process.env.TOKEN_EXPIRY_TIME), success: true})
 					} else {
-						return res.status(400).json({message : 'Email or Password is incorrect', success : false})
+						return res.status(400).json({message: 'Email or Password is incorrect', success: false})
 					}
 				})
 			} else {
-				res.status(400).json({message : 'Email does not exists', success : false})
+				res.status(400).json({message: 'Email does not exists', success: false})
 			}
 		})
 	}
@@ -85,8 +89,8 @@ users.post('/login',
 
 users.post('/logout', (req, res) => {
 	req.session.destroy((err) => {
-	  	if (err) { return res.status(200).json({message : 'Unable to logout', err, success : false}) }
-    	res.status(200).json({message : 'user successfully logout', success : true})
+	  	if (err) { return res.status(200).json({message: 'Unable to logout', err, success: false}) }
+    	res.status(200).json({message: 'User successfully logout', success: true})
 	})
 })
 
@@ -107,15 +111,15 @@ users.get('/downloadSampleExcel', (req, res) => {
 
 users.post('/resetPassword', 
 	[
-		check('currentPassword').not().isEmpty().withMessage('currentPassword cannot be empty'),
-		check('newPassword').not().isEmpty().withMessage('newPassword cannot be empty'),
+		check('currentPassword').not().isEmpty().withMessage('Current password cannot be empty. Please enter your current password'),
+		check('newPassword').not().isEmpty().withMessage('New password cannot be empty. Please enter your new password'),
 		check('newPassword').isLength({ min: 8 }).withMessage('New password must be atleast 8 characters in length'),
-		check('confirmNewPassword').not().isEmpty().withMessage('confirmNewPassword cannot be empty')
+		check('confirmNewPassword').not().isEmpty().withMessage('Confirm new password cannot be empty. Please enter your confirm new password')
 	],
 	(req,res) => {
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
-		    return res.status(400).json({message: errors.array()})
+		    return res.status(400).json({message: errors.array()[0].msg, success: false})
 	  	}
 
 	  	const { currentPassword, newPassword, confirmNewPassword } = req.body
@@ -123,27 +127,27 @@ users.post('/resetPassword',
 
 	  	if(newPassword === confirmNewPassword) {
 		  	connection.query(`SELECT PASSWORD FROM ${process.env.USER_TBL} WHERE ROLE = ? AND STATUS = 1`, [userRole], (err, results, fields) => {
-				if (err) { return res.status(400).json({message : 'Unable to reset password. Please try again', success : false}) }
+				if (err) { return res.status(400).json({message: 'Unable to reset password. Please try again', success: false}) }
 
 				if (results.length > 0) {
 					bcrypt.compare(currentPassword, results[0].PASSWORD).then((match) => {
 						if (match == true) {
 							bcrypt.hash(newPassword, parseInt(process.env.SALT_ROUNDS)).then((hashedPassword) => {
 								connection.query(`UPDATE ${process.env.USER_TBL} SET PASSWORD = ?`, [hashedPassword], (err, results, fields) => {
-									if (err) { return res.status(400).json({message : 'Unable to reset password. Please try again', success : false}) }
-									res.status(200).json({message : 'User password resetted successfully', success: true})	
+									if (err) { return res.status(400).json({message: 'Unable to reset password. Please try again', success: false}) }
+									res.status(200).json({message: 'User password resetted successfully', success: true})	
 								})
 							})
 						} else {
-							return res.status(400).json({message : 'Incorrect password sent. Please enter the correct password', success : false})
+							return res.status(400).json({message: 'Incorrect password sent. Please enter the correct password', success: false})
 						}
 					})
 				} else {
-					return res.status(400).json({message : 'No user exists', success : false})
+					return res.status(400).json({message: 'No user exists', success: false})
 				}
 		  	})
 	  	} else {
-	  		res.status(400).json({message : 'Password and confirm password do not match. Please try again', success : false})
+	  		res.status(400).json({message:  'Password and confirm password do not match. Please try again', success: false})
 	  	}
 	}
 )
