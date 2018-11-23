0a366eb634b8abc57fa02b3c2b7f7d2601a60306
Abhishek Thakur <coach.ab9@gmail.com>
2018-11-24 02:08:43 +0530

    #bug fixes.

--- src/messages.js
+++ src/messages.js
@@ -169,5 +169,175 @@ export default {
 			}
 		}
 	},
+	'en-GB': {
+		common: {
+			filter: {
+				title: '',
+				mainHeading: '',
+				searchPanelHeading: '',
+				searchBoxHeading: '',
+				searchBoxPlaceholder: '',
+				filterBtn: '',
+				resetBtn: '',
+				quickSearchPlaceHolder: ''
+			},
+			navbar: {
+				servicePanelLink: '',
+				resetPasswordLink: '',
+				logoutLink: ''
+			},
+			loginForm: {
+				moduleTitle: '',
+				emailField: '',
+				passwordField: '',
+				loginBtn: ''
+			},
+			errorPage: {
+				msg1: '',
+				msg2: '',
+				msg3: ''
+			},
+			servicePanel: {
+				title: '',
+				fileRecordTitle: '',
+				letterRecordTitle: ''
+			}
+		},
+		fileManager: {
+			sidebar: {
+				addNewRecord: '',
+				manageRecords: '',
+				getRecords: '',
+				importExcel: '',
+				manageSystemInputs: ''
+			},
+			infoboard: {
+				total: '',
+				pending: '',
+				approved: '',
+				rejected: '',
+				chartHolderTitle: ''
+			},
+			getRecords: {
+				byMonthSection: {
+					title: '',
+					year: '',
+					month: '',
+					errMsg: ''
+				},
+				specificPeriodSection: {
+					title: '',
+					from: '',
+					to: '',
+					errMsg: ''
+				},
+				byTagsSection: {
+					title: ''
+				},
+				selectFormatSection: {
+					title: '',
+					excel: '',
+					pdf: '',
+					errMsg: ''
+				},
+				fetchDataBtn: '',
+				downloadExcelLink: '',
+				downloadPdfLink: '',
+			},
+			import: {
+				dwnldSampleExcelMsg: '',
+				importDataBtn: '',
+				dropzone: {
+					msg1: '',
+					msg2: '',
+					uploadFileBtn: ''
+				}
+			},
+			record: {
+				dropdown: {
+					btn1: '',
+					btn2: '',
+					btn3: ''
+				},
+				editRecordModal: {
+					title: ''
+				},
+				deleteRecordModal: {
+					title: '',
+					warning: '',
+					closeBtn: '',
+					deleteBtn: ''
+				},
+				manageRecordModal: {
+					title: ''
+				}
+			}
+		},
+		letterTracking: {
+			sidebar: {
+				addNewEntry: '',
+				manageLetters: '',
+				exportData: '',
+				manage: ''
+			},
+			letterboard: {
+				total: '',
+				incoming: '',
+				outgoing: '',
+				chartHolderTitle: ''
+			},
+			newLetterEntryForm: {
+				regNo: '',
+				status: '',
+				date: '',
+				deptName: '',
+				type: '',
+				tag: '',
+				assignedOfficer: '',
+				address: '',
+				subject: '',
+				remark: '',
+				submitBtnSaveText: '',
+				submitBtnUpdateText: '',
+				saveEntryFormLabel: '',
+				updateEntryFormLabel: '',
+				dropzone: {
+					msg1: '',
+					msg2: '',
+					uploadBtn: '',
+					uploadFileErrMsg1: '',
+					uploadFileErrMsg2: '',
+					uploadFileErrMsg3: '',
+					removeGlyphTitle: ''
+				}
+			},
+			exportLetterData: {
+				byMonthSection: {
+					title: '',
+					year: '',
+					month: '',
+					errMsg: ''
+				},
+				specificPeriodSection: {
+					title: '',
+					from: '',
+					to: '',
+					errMsg: ''
+				},
+				byTagsSection: {
+					title: ''
+				},
+				selectFormatSection: {
+					title: '',
+					excel: '',
+					pdf: '',
+					errMsg: ''
+				},
+				fetchDataBtn: '',
+				downloadExcelLink: '',
+				downloadPdfLink: '',
+			}
+		}
+	},
 	'hi-IN': {}
 }
